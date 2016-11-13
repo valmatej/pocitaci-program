30aa4711284362d6b8c93af16d66bc2447f6ca56
Matej Valasek <valmatej@seznam.cz>
2016-11-13 11:18:15 +0100

    přidán přehrávač

---
+++ mujprehravac.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+MUSIC_DIRECTORY = "/home/mates/Plocha/Valmatej/hudba/Music"
+from time import sleep
+from os import listdir
+from os.path import isfile, join
+import subprocess
+
+#funkce která vrátí seznam mp3 souborů z určitého adresáře
+def get_files (directory):
+    mp3_files = []
+    for f in listdir(directory):
+        if isfile(join(directory, f)) and f[-4:] == ".mp3":
+            mp3_files.append(f)
+    return sorted(mp3_files)
+
+def zobrazeni_vyzvy(pocet_pisnicek):
+    vyzva = "vyberte si který soubor chcete přehrát:"
+    while True:
+        try:
+            cislo_pisnicky = int(input(vyzva))
+            if cislo_pisnicky > 0 and cislo_pisnicky <= pocet_pisnicek:
+                break
+        except:
+            pass
+        print("  mělo to být číslo,od 1 do", pocet_pisnicek)
+    return cislo_pisnicky
+
+mp3_files = get_files(MUSIC_DIRECTORY)
+
+for i, f in enumerate(mp3_files, start=1):
+    print(i,f)
+
+cislo_vybrane_pisnicky = zobrazeni_vyzvy(len(mp3_files))
+vybrany_soubor = mp3_files[cislo_vybrane_pisnicky -1]
+print ("vybrali jste písničku číslo:", cislo_vybrane_pisnicky, vybrany_soubor)
+
+subprocess.run(["mpg123", join(MUSIC_DIRECTORY, vybrany_soubor)])
+sleep(2)
+
+
+
+
+
+
+
+
+
+
+
+
