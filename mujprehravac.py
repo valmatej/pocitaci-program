a6b9b9a3becec66366438b611a4b32a7430d3e96
Matej Valasek <valmatej@seznam.cz>
2016-12-29 16:08:50 +0100

    zredukování kódu přidání while True na začátek skriptu

--- mujprehravac.py
+++ mujprehravac.py
@@ -67,66 +67,42 @@ from os import listdir
 from os.path import isfile, join
 import subprocess
 
+
 #funkce která vrátí seznam mp3 souborů z určitého adresáře
-def get_files (directory):
-    mp3_files = []
-    for f in listdir(directory):
-        if isfile(join(directory, f)) and f[-4:] == ".mp3":
-            mp3_files.append(f)
-    return sorted(mp3_files)
-
-def zobrazeni_vyzvy(pocet_pisnicek):
-    vyzva = "Choose the file you want to play:"
-    while True:
-        try:
-            cislo_pisnicky = int(input(vyzva))
-            if cislo_pisnicky > 0 and cislo_pisnicky <= pocet_pisnicek:
-                break
-        except:
-            pass
-        print("  It should be a number from 1 to", pocet_pisnicek)
-    return cislo_pisnicky
-
-mp3_files = get_files(MUSIC_DIRECTORY)
-
-for i, f in enumerate(mp3_files, start=1):
-    print(i,f)
-
-cislo_vybrane_pisnicky = zobrazeni_vyzvy(len(mp3_files))
-vybrany_soubor = mp3_files[cislo_vybrane_pisnicky -1]
-print ("You chose a song number:", cislo_vybrane_pisnicky, vybrany_soubor)
-print(" ")
-print(" ")
-
-subprocess.run(["mpg123", join(MUSIC_DIRECTORY, vybrany_soubor)])
 while True:
-    print(" ")
-    print(" ")
-    def zobrazeni_vyzvy(pocet_pisnicek):
-        vyzva = "Choose the file you want to play:"
-        while True:
-            try:
-                cislo_pisnicky = int(input(vyzva))
-                if cislo_pisnicky > 0 and cislo_pisnicky <= pocet_pisnicek:
-                    break
-            except:
-                pass
-            print("  It should be a number from 1 to", pocet_pisnicek)
-        return cislo_pisnicky
+    def get_files (directory):
+        mp3_files = []
+        for f in listdir(directory):
+            if isfile(join(directory, f)) and f[-4:] == ".mp3":
+                mp3_files.append(f)
+        return sorted(mp3_files)
+
+    while True:
+        def zobrazeni_vyzvy(pocet_pisnicek):
+            vyzva = "Choose the file you want to play:"
+            while True:
+                try:
+                    cislo_pisnicky = int(input(vyzva))
+                    if cislo_pisnicky > 0 and cislo_pisnicky <= pocet_pisnicek:
+                        break
+                except:
+                    pass
+                print("  It should be a number from 1 to", pocet_pisnicek)
+            return cislo_pisnicky
 
 
-    mp3_files = get_files(MUSIC_DIRECTORY)
+        mp3_files = get_files(MUSIC_DIRECTORY)
 
-    for i, f in enumerate(mp3_files, start=1):
-        print(i, f)
+        for i, f in enumerate(mp3_files, start=1):
+            print(i, f)
 
-    cislo_vybrane_pisnicky = zobrazeni_vyzvy(len(mp3_files))
-    vybrany_soubor = mp3_files[cislo_vybrane_pisnicky - 1]
-    print("You chose a song number:", cislo_vybrane_pisnicky, vybrany_soubor)
-    print(" ")
-    print(" ")
+        cislo_vybrane_pisnicky = zobrazeni_vyzvy(len(mp3_files))
+        vybrany_soubor = mp3_files[cislo_vybrane_pisnicky - 1]
+        print("You chose a song number:", cislo_vybrane_pisnicky, vybrany_soubor)
+        print(" ")
+        print(" ")
 
-    subprocess.run(["mpg123", join(MUSIC_DIRECTORY, vybrany_soubor)])
+        subprocess.run(["mpg123", join(MUSIC_DIRECTORY, vybrany_soubor)])
 
 
 
